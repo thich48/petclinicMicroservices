f2036e55c75a802ad33ac83ada5c65989134b119
Hicham <7572448+thich48@users.noreply.github.com>
2023-06-16 12:20:18 +0200

    upd

--- infrastructure/create-ecr-docker-registry-for-dev.sh
+++
@@ -1,9 +0,0 @@
-PATH="$PATH:/usr/local/bin"
-APP_REPO_NAME="thich48-repo/petclinic-app-dev"
-AWS_REGION="eu-west-1"
-
-aws ecr create-repository \
-  --repository-name ${APP_REPO_NAME} \
-  --image-scanning-configuration scanOnPush=false \
-  --image-tag-mutability MUTABLE \
-  --region ${AWS_REGION}
